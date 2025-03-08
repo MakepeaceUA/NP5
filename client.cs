d926985587eb4731b5e4093afcca8a47383436c7
Arsen Sevastyanov <130586920+MakepeaceUA@users.noreply.github.com>
2025-03-08 21:19:07 +0100

    Add files via upload

---
+++ client.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Sockets;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ConsoleApp40
+{
+    internal class Program
+    {
+        static async Task Main()
+        {
+            try
+            {
+                using (TcpClient client = new TcpClient())
+                {
+                    await client.ConnectAsync("127.0.0.1", 5000);
+                    Console.WriteLine("Подключение к серверу...");
+
+                    NetworkStream stream = client.GetStream();
+                    byte[] buffer = new byte[1024];
+                    int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+                    string weatherData = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                    Console.WriteLine("Данные о погоде получены:");
+                    Console.WriteLine(weatherData.Substring(0, weatherData.Length));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ server.cs
@@ -0,0 +1,44 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace ConsoleApp39
+{
+    internal class Program
+    {
+        static async Task Main()
+        {
+            TcpListener server = new TcpListener(IPAddress.Any, 5000);
+            server.Start();
+            Console.WriteLine("Сервер запущен, ожидание подключения клиента...");
+
+            while (true)
+            {
+                TcpClient client = await server.AcceptTcpClientAsync();
+                Console.WriteLine("Клиент подключился.");
+
+                try
+                {
+                    string url = "https://openweathermap.org/city/698740"; 
+                    using (HttpClient httpClient = new HttpClient())
+                    {
+                        string pageContent = await httpClient.GetStringAsync(url);
+                        byte[] data = Encoding.UTF8.GetBytes(pageContent);
+
+                        NetworkStream stream = client.GetStream();
+                        await stream.WriteAsync(data, 0, data.Length);
+                        Console.WriteLine("Данные о погоде отправлены клиенту.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Ошибка: {ex.Message}");
+                }
+                finally
+                {
+                    client.Close();
+                }
+            }
+        }
+    }
+}
